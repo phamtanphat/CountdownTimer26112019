234c00cc804a93d661ec6e847b23aea174bb469d
phatdroid94 <phatdroid94@gmail.com>
2019-12-19 19:19:15 +0700

    finish

--- app/src/main/java/com/example/countdowntimer26112019/MainActivity.java
+++ app/src/main/java/com/example/countdowntimer26112019/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,39 +18,58 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.image_2,
             R.drawable.image_3
     };
-    ImageView mImgBanner;
+//    ImageView mImgBanner;
+//    Button mBtnStart;
+//    int mCount = 0;
+//    boolean mIsLoading = false;
     Button mBtnStart;
-    int mCount = 0;
-    boolean mIsLoading = false;
-
+    ViewFlipper mViewFlipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//        mBtnStart = findViewById(R.id.buttonStartBanner);
+//        mImgBanner = findViewById(R.id.imageviewBanner);
+//
+//        mBtnStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (mIsLoading) {
+//                    return;
+//                }
+//                mIsLoading = true;
+//                CountDownTimer countDownTimer = new CountDownTimer((mArrayImages.length * 1000) + 1000, 1000) {
+//                    @Override
+//                    public void onTick(long millisUntilFinished) {
+//                        mImgBanner.setImageResource(mArrayImages[mCount++]);
+//                    }
+//                    @Override
+//                    public void onFinish() {
+//                        mCount = 0;
+//                        this.start();
+//                    }
+//                };
+//                countDownTimer.start();
+//            }
+//        });
         mBtnStart = findViewById(R.id.buttonStartBanner);
-        mImgBanner = findViewById(R.id.imageviewBanner);
+        mViewFlipper = findViewById(R.id.viewflpper);
+
+        for (int i = 0 ; i < mArrayImages.length ; i++){
+            ImageView imageView = new ImageView(this);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageView.setImageResource(mArrayImages[i]);
+            mViewFlipper.addView(imageView);
+        }
 
         mBtnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mIsLoading) {
+                if (mViewFlipper.isFlipping()){
                     return;
                 }
-                mIsLoading = true;
-                CountDownTimer countDownTimer = new CountDownTimer((mArrayImages.length * 1000) + 1000, 1000) {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-                        mImgBanner.setImageResource(mArrayImages[mCount++]);
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        mCount = 0;
-                        this.start();
-                    }
-                };
-                countDownTimer.start();
+                mViewFlipper.setFlipInterval(1000);
+                mViewFlipper.startFlipping();
             }
         });
 
